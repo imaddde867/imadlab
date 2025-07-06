96025987756e2171d66976440b1697a7d848f900
imaddde867 <63160938+imaddde867@users.noreply.github.com>
2025-07-06 19:02:17 +0300

    fix: back from HashRouter to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { Suspense } from "react";
 import ClickSpark from "@/components/ClickSpark";
 
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
           <ClickSpark
             sparkColor="#fff"
@@ -40,7 +40,7 @@ const App = () => (
             </Routes>
           </ClickSpark>
         </Suspense>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
