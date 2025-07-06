407842b7901794dbbaaaabbcb50fa76e19ba888f
imaddde867 <63160938+imaddde867@users.noreply.github.com>
2025-07-06 14:59:17 +0300

    feat: Add fade-in animation to 404 page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,11 +86,16 @@ export default {
 					to: {
 						height: '0'
 					}
+				},
+				'fade-in': {
+					from: { opacity: '0' },
+					to: { opacity: '1' }
 				}
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out'
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'fade-in': 'fade-in 1s ease-out forwards'
 			}
 		}
 	},
