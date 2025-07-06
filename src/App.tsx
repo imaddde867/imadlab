637440a4876846d5a3bf1efbc3086ee9ffb4f860
imaddde867 <63160938+imaddde867@users.noreply.github.com>
2025-07-06 18:35:56 +0300

    fix: replace BrowserRouter with HashRouter for improved routing compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import React, { Suspense } from "react";
 import ClickSpark from "@/components/ClickSpark";
 
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Suspense fallback={<div>Loading...</div>}>
           <ClickSpark
             sparkColor="#fff"
@@ -40,7 +40,7 @@ const App = () => (
             </Routes>
           </ClickSpark>
         </Suspense>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
